8e4c5f8955c15b06f72eedf2411dc8dd995341aa
Tran Nguyen Minh Quan <101453916+106210137minhquan@users.noreply.github.com>
2022-03-12 17:18:38 +0700

    Add files via upload

---
+++ Baitap1.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << "Ho va ten: Tran Nguyen Minh Quan " << '\n'
+         << "MSSV: 106210137" << '\n'
+         << "Lop: 21DTCLC2";
+    return 0;
+}
